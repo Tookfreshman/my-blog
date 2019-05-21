2467db4376c191999c9f37d5b1a5f36d1ff3ac77
Took <81023424@qq.com>
2019-05-21 16:56:02 +0800

    提交

---
+++ src/components/CommonHeader/index.js
@@ -0,0 +1,13 @@
+import './index.less'
+import React, { Component } from 'react'
+import { Row, Col } from 'antd'
+
+export default class CommonHeader extends Component {
+  render() {
+    return (
+      <Row>
+        <Col span={24}>123123123123</Col>
+      </Row>
+    )
+  }
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import './index.less'
+import React, { Component } from 'react'
+import CommonHeader from '@/components/CommonHeader'
+
+export default class Home extends Component {
+  render() {
+    return <CommonHeader />
+  }
+}
